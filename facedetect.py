cbb42fe7e3f260648d8ca2ba8fb1671ad0bc5e5d
aashish subedi <aashishsubedi105@gmail.com>
2024-07-23 12:53:18 +0530

    changed maxpooling 2->3

--- facedetect.py
+++ facedetect.py
@@ -10,7 +10,7 @@ model = tf.keras.Sequential([
     tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
     tf.keras.layers.Conv2D(128, kernel_size=(3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 3)),
     tf.keras.layers.Dropout(0.3),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(7, activation='softmax')
--- facedetect2.py
+++ facedetect2.py
@@ -10,7 +10,7 @@ model = keras.Sequential([
     keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
     keras.layers.MaxPooling2D(pool_size=(2, 2)),
     keras.layers.Conv2D(128, kernel_size=(3, 3), activation='relu'),
-    keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    keras.layers.MaxPooling2D(pool_size=(2, 3)),
     keras.layers.Dropout(0.3),
     keras.layers.Flatten(),
     keras.layers.Dense(7, activation='softmax')
